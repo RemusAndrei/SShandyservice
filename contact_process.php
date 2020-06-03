f44fad15da139ca45f686d098b400b769fa0794b
Remus Cicu <remuscicu@gmail.com>
2020-06-03 14:30:53 +0200

    setting up the colors

--- contact_process.php
+++ contact_process.php
@@ -1,19 +1,20 @@
 <?php
 
-    $to = "remus.a.cicu@gmail.com";
+    $to = 'remus.a.cicu@gmail.com';
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
+    $number=$_REQUEST['number'];
     $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
+	$btype = $_REQUEST['btype'];
     $cmessage = $_REQUEST['message'];
 
     $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+	  $headers = "From: " . $from . "\r\n";
+	  $headers .= "Reply-To: ". $from . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Bitmap Photography.";
+    $subject = "Hansen Rengøring";
 
     $logo = 'img/logo.png';
     $link = '#';
@@ -23,10 +24,11 @@
 	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
 	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
 	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+	$body .= "<td style='border:none;'><strong>Navn:</strong> {$name}</td>";
 	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+	$body .= "<td style='border:none;'><strong>Type of business:</strong> {$btype}</td>";
 	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
 	$body .= "<tr><td></td></tr>";
 	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
 	$body .= "</tbody></table>";
